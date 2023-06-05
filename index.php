209a696b4ccba3fcf9a1a2096b9649a862619115
Saiti91 <106150763+Saiti91@users.noreply.github.com>
2023-06-05 14:48:50 +0200

    1

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Home</title>
+    <link rel="stylesheet" href="bootstrap-5.3.0-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="slider.js"></script>
+  </head>
+
+  <body>
+    <header>
+      <div class="menu">
+        <div class="logo">
+          <img src="asset/Logo.svg" height="50px" width="50px" alt="logo" />
+        </div>
+        <a href=""
+          ><svg
+            height="2.5rem"
+            style="height: 2.5rem"
+            version="1.1"
+            id="Icon_soleil"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 300 300"
+            style="enable-background: new 0 0 300 300"
+            xml:space="preserve"
+          >
+            <style type="text/css">
+              .st0 {
+                fill: #ffffff;
+              }
+            </style>
+            <g>
+              <g>
+                <path
+                  class="st0"
+                  d="M149.5,290.94c-19.06,0-37.55-3.73-54.95-11.1c-16.81-7.12-31.91-17.31-44.87-30.28
+				   c-12.97-12.98-23.15-28.09-30.26-44.92c-7.36-17.42-11.09-35.92-11.09-55c0-19.08,3.73-37.58,11.09-55
+				   c7.11-16.83,17.29-31.94,30.26-44.92c12.97-12.98,28.06-23.17,44.87-30.28c17.4-7.37,35.89-11.1,54.95-11.1s37.55,3.74,54.95,11.1
+				   c16.81,7.12,31.91,17.31,44.87,30.28c12.97,12.98,23.15,28.09,30.26,44.92c7.36,17.42,11.09,35.92,11.09,55
+				   c0,19.08-3.73,37.58-11.09,55c-7.11,16.83-17.29,31.94-30.26,44.92c-12.97,12.98-28.06,23.17-44.87,30.28
+				   C187.05,287.21,168.56,290.94,149.5,290.94z"
+                />
+                <path
+                  d="M149.5,15.68c18.07,0,35.6,3.54,52.09,10.52c7.93,3.36,15.57,7.51,22.73,12.35c7.09,4.79,13.76,10.3,19.82,16.37
+				   c6.06,6.07,11.56,12.74,16.35,19.84c4.83,7.16,8.98,14.81,12.34,22.75c6.97,16.51,10.51,34.05,10.51,52.14
+				   s-3.54,35.63-10.51,52.14c-3.35,7.93-7.5,15.59-12.34,22.75c-4.79,7.1-10.29,13.77-16.35,19.84
+				   c-6.06,6.07-12.73,11.57-19.82,16.37c-7.15,4.84-14.8,8.99-22.73,12.35c-16.49,6.98-34.02,10.52-52.09,10.52
+				   s-35.6-3.54-52.09-10.52c-7.93-3.36-15.57-7.51-22.73-12.35c-7.09-4.79-13.76-10.3-19.82-16.37
+				   c-6.06-6.07-11.56-12.74-16.35-19.84c-4.83-7.16-8.98-14.81-12.34-22.75c-6.97-16.51-10.51-34.05-10.51-52.14
+				   s3.54-35.63,10.51-52.14c3.35-7.93,7.5-15.59,12.34-22.75c4.79-7.1,10.29-13.77,16.35-19.84c6.06-6.07,12.73-11.57,19.82-16.37
+				   c7.15-4.84,14.8-8.99,22.73-12.35C113.9,19.22,131.43,15.68,149.5,15.68 M149.5,1C67.49,1,1,67.55,1,149.64
+				   s66.49,148.64,148.5,148.64S298,231.73,298,149.64S231.51,1,149.5,1L149.5,1z"
+                />
+              </g>
+            </g>
+            <g>
+              <g>
+                <ellipse
+                  class="st0"
+                  cx="149.72"
+                  cy="149.85"
+                  rx="43.23"
+                  ry="43.27"
+                />
+                <path
+                  d="M149.72,113.92c19.79,0,35.9,16.12,35.9,35.93s-16.1,35.93-35.9,35.93c-19.79,0-35.9-16.12-35.9-35.93
+				   S129.92,113.92,149.72,113.92 M149.72,99.24c-27.88,0-50.56,22.7-50.56,50.61s22.68,50.61,50.56,50.61
+				   c27.88,0,50.56-22.7,50.56-50.61S177.6,99.24,149.72,99.24L149.72,99.24z"
+                />
+              </g>
+            </g>
+            <path
+              d="M149.5,38.84L149.5,38.84c-4.3,0-7.78,3.48-7.78,7.78v29.53c0,4.3,3.48,7.78,7.78,7.78h0c4.3,0,7.78-3.48,7.78-7.78V46.62
+		   C157.28,42.32,153.8,38.84,149.5,38.84z"
+            />
+            <path
+              d="M149.5,215.72L149.5,215.72c-4.3,0-7.78,3.48-7.78,7.78v29.53c0,4.3,3.48,7.78,7.78,7.78h0c4.3,0,7.78-3.48,7.78-7.78V223.5
+		   C157.28,219.21,153.8,215.72,149.5,215.72z"
+            />
+            <path
+              d="M260.38,149.64L260.38,149.64c0-4.3-3.49-7.79-7.79-7.79h-29.47c-4.3,0-7.79,3.49-7.79,7.79v0c0,4.3,3.49,7.79,7.79,7.79
+		   h29.47C256.89,157.43,260.38,153.94,260.38,149.64z"
+            />
+            <path
+              d="M83.66,149.64L83.66,149.64c0-4.3-3.49-7.79-7.79-7.79H46.41c-4.3,0-7.79,3.49-7.79,7.79v0c0,4.3,3.49,7.79,7.79,7.79h29.47
+		   C80.18,157.43,83.66,153.94,83.66,149.64z"
+            />
+            <path
+              d="M227.9,71.53L227.9,71.53c-3.04-3.04-7.96-3.04-11,0l-20.85,20.87c-3.04,3.04-3.04,7.97,0,11.01v0c3.04,3.04,7.96,3.04,11,0
+		   l20.85-20.87C230.94,79.5,230.94,74.57,227.9,71.53z"
+            />
+            <path
+              d="M102.94,196.6L102.94,196.6c-3.04-3.04-7.96-3.04-11,0L71.1,217.47c-3.04,3.04-3.04,7.97,0,11.01l0,0
+		   c3.04,3.04,7.96,3.04,11,0l20.85-20.87C105.98,204.57,105.98,199.64,102.94,196.6z"
+            />
+            <path
+              d="M228.03,228.35L228.03,228.35c3.04-3.04,3.04-7.97,0-11.01l-20.85-20.87c-3.04-3.04-7.96-3.04-11,0h0
+		   c-3.04,3.04-3.04,7.97,0,11.01l20.85,20.87C220.07,231.39,224.99,231.39,228.03,228.35z"
+            />
+            <path
+              d="M103.07,103.28L103.07,103.28c3.04-3.04,3.04-7.97,0-11.01L82.22,71.4c-3.04-3.04-7.96-3.04-11,0h0
+		   c-3.04,3.04-3.04,7.97,0,11.01l20.85,20.87C95.11,106.32,100.04,106.32,103.07,103.28z"
+            />
+          </svg>
+        </a>
+        <a href="" id="hmenu"><strong>Home</strong></a>
+        <a href="" id="hmenu"><strong>News</strong></a>
+        <a href="" target="_blank" id="hmenu"><strong>Forum</strong></a>
+        <a href="" target="_blank" id="hmenu"><strong>Shop</strong></a>
+        <a href="" class="btn" id="BurgerMenu">Login</a>
+      </div>
+      <!--
+      <div class="Burger menu">
+        <svg
+          height="2.5rem"
+          style="height: 2.5rem"
+          version="1.1"
+          id="BurgerMenu"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 300 300"
+          style="enable-background: new 0 0 300 300"
+          xml:space="preserve"
+        >
+          <style type="text/css">
+            .st0 {
+              fill: #ffffff;
+              stroke: #000000;
+              stroke-miterlimit: 10;
+            }
+          </style>
+          <g>
+            <path
+              class="st0"
+              d="M151.21,267.31c-38.97,0-77.94,0.03-116.91-0.02c-12.8-0.01-21.86-6.86-24.63-18.38
+                c-3.41-14.16,7.14-28.22,21.74-28.97c0.72-0.04,1.45-0.02,2.17-0.02c78.37,0,156.75-0.01,235.12,0.01
+                c10.87,0,19.6,6.07,23.05,15.89c3.34,9.52,0.42,19.78-7.47,26.23c-4.97,4.06-10.76,5.29-17.04,5.28
+                C228.57,267.29,189.89,267.31,151.21,267.31z"
+            />
+            <path
+              class="st0"
+              d="M151.15,30.5c38.97,0,77.94-0.02,116.92,0.01c12.23,0.01,21.9,7.28,24.4,18.23
+                c3.38,14.78-7.02,28.38-22.38,29.25c-0.87,0.05-1.74,0.03-2.61,0.03c-77.65,0-155.31,0.01-232.96-0.01
+                C20.37,78,9.84,68.82,9.05,55.92c-0.87-14.21,9.92-25.37,24.75-25.4c32.89-0.05,65.77-0.02,98.66-0.02
+                C138.69,30.5,144.92,30.5,151.15,30.5z"
+            />
+            <path
+              class="st0"
+              d="M150.96,125.21c39.26,0,78.52-0.05,117.78,0.02c17.46,0.03,29,16.68,22.7,32.49
+                c-3.69,9.26-12.13,14.86-22.91,14.93c-15.21,0.09-30.42,0.03-45.63,0.03c-62.73,0-125.46,0.01-188.19-0.01
+                c-12.46,0-21.74-6.5-24.76-17.19c-4.35-15.4,6.93-30.21,23.23-30.24C72.44,125.16,111.7,125.21,150.96,125.21z"
+            />
+          </g>
+        </svg>
+      </div>-->
+    </header>
+    <main>
+      <section>
+        <div id="slider">
+          <div class="img">
+            <img src="asset/concert1.webp" alt="concert1" id="slide" />
+          </div>
+          <div id="precedent" onclick="ChangeSlide(-1)">&lt;</div>
+          <div id="suivant" onclick="ChangeSlide(1)">&gt;</div>
+        </div>
+        <a href="evenement.html" id="lien-evenement">See more</a>
+      </section>
+
+      <section class="products" id="products">
+        <div class="heading">
+          <h2>Category we have</h2>
+        </div>
+
+        <div class="category">
+          <div id="c">
+            <a href="article.html#classical" class="btn rounded-pill"
+              >Classical</a
+            >
+          </div>
+
+          <div id="c">
+            <a href="article.html#country" class="btn rounded-pill">Country</a>
+          </div>
+
+          <div id="c">
+            <a href="article.html#jazz" class="btn rounded-pill">Jazz</a>
+          </div>
+
+          <div id="c">
+            <a href="article.html#pop" class="btn rounded-pill">Pop</a>
+          </div>
+
+          <div id="c">
+            <a href="article.html#metal" class="btn rounded-pill">Rock</a>
+          </div>
+        </div>
+
+        <div class="products-container">
+          <div class="box">
+            <img class="image" src="asset\Dua lip.jpg" alt="image" />
+            <p id="d">july 2002</p>
+            <h2 id="title">Dua lipa the star of the moment.</h2>
+            <p id="description">
+              The star who is making a hit right now with her best hit of the
+              century released in 2022 this summer.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+
+          <div class="box">
+            <img class="image" src="asset\bg mendes.jpg" alt="image" />
+            <p id="d">jun 2022</p>
+            <h2 id="title">Shawn Mendes the new born.</h2>
+            <p id="description">
+              The star who is making a hit right now with her best hit of the
+              century released in 2022 this summer.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+
+          <div class="box">
+            <img class="image" src="asset\Angèle.jpg" alt="image" />
+            <p id="d">october 2023</p>
+            <h2 id="title">Angel the French star of the moment</h2>
+            <p id="description">
+              The star who is making a hit right now with her best hit of the
+              century released in 2022 this summer.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+
+          <div class="box">
+            <img class="image" src="asset\marilyne5.jpg" alt="image" />
+            <p id="d">Mai 2022</p>
+            <h2 id="title">Marilyn Monroe: The Woman Behind the Myth"</h2>
+            <p id="description">
+              Marilyn Monroe was an actress remains an emblematic figure of
+              popular culture, the unforgatable star.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+          <div class="box">
+            <img class="image" src="asset\Elvis Presley.jpg" alt="image" />
+            <p id="d">september 2023</p>
+            <h2 id="title">Elvis Presley: The Man, the Music, the Legend</h2>
+            <p id="description">
+              Elvis became a cultural phenomenon, revolutionizing popular music
+              and rock and roll artists.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+          <div class="box">
+            <img
+              class="image"
+              src="asset\Madonna (illuminati symbolism).jpg"
+              alt="image"
+            />
+            <p id="d">july 2022</p>
+            <h2 id="title">Madonna</h2>
+            <p id="description">
+              Madonna is an American singer, songwriter, actress, and
+              businesswoman who has achieved massive success throughout her
+              career.
+            </p>
+            <div class="content">
+              <a href="article.html" target="_blank" class="btn rounded-pill"
+                >Read More</a
+              >
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="end-products">
+        <p>
+          Visit our Articles page to explore more <br />captivating content and
+          dive deeper into our collection of articles.
+        </p>
+        <a href="article.html" class="btn">See more</a>
+      </section>
+
+      <section>
+        <h1 id="titre">Visit Our Store</h1>
+        <section class="produits">
+          <div class="card">
+            <div class="card-image img-1"></div>
+            <h2>The Weekend Sweat<br />31.99 €</h2>
+            <a href="shop.html">Buy now</a>
+          </div>
+
+          <div class="card">
+            <div class="card-image img-2"></div>
+            <h2>Ariana Phone Case <br />18.90 €</h2>
+            <a href="shop.html">Buy now</a>
+          </div>
+
+          <div class="card">
+            <div class="card-image img-3"></div>
+            <h2>Shawn Mendes Cap<br />25.99 €</h2>
+
+            <a href="shop.html">Buy now</a>
+          </div>
+        </section>
+
+        <a href="shop.html" id="acheter">Shop at our store</a>
+      </section>
+
+      <section class="suscribe">
+        <div class="S">
+          <p>
+            Interested to get notified ? <br />
+            Suscribe and get <span>the most</span> piping
+            <span>hot news</span> of the week, emailed straight to your inbox.
+          </p>
+          <a href="newsletter.html" class="btn2">Suscribe</a>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <div class="footer-content">
+        <h3>Holomusic</h3>
+        <p>
+          Unleash Your Imagination: <br />Explore the Cutting-Edge World of
+          HoloMusic Articles!
+        </p>
+      </div>
+      <hr />
+
+      <div class="footer-bottom">
+        <p>Copyright &copy; 2023 HOLOMUSIC</p>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ slider.js
@@ -0,0 +1,14 @@
+
+const slide = ["concert1.webp", "chanteur.webp", "chanteur1.webp", "concert2.webp"];
+let num = 0;
+function ChangeSlide(sens){
+    num = num + sens;
+    if(num > 3)
+    num = 0;
+    if(num < 0)
+    num=3;
+     document.getElementById("slide").src ="asset/" + slide[num];
+
+}
+setInterval("ChangeSlide(1)", 3000);
+
