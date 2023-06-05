b79ccfde7861a2fdea28ffed4bd4671deb3d1b72
Saiti91 <106150763+Saiti91@users.noreply.github.com>
2023-06-05 15:00:16 +0200

    2

---
+++ includes/header_backoffice.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+            integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+            crossorigin="anonymous"></script>
+    <link type="text/css" href="style.css" rel="stylesheet">
+    <title>Users Management</title>
+</head>
+<body class="bg-dark text-bg-dark">
+<header class="bg-secondary">
+    <h1>Back Office</h1>
+    <nav class="navbar-nav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="../Sell_Sheet.php">Sells Sheet</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../User_Management.php">Users Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Articles Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Events Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Comments Management</a>
+            </li>
+
+        </ul>
+    </nav>
+</header>
---
+++ includes/header_index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Home</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+            integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css"/>
+    <script src="slider.js"></script>
+</head>
+
+<body>
+<header class="container-fluid header">
+    <div class="logo">
+        <img src="asset/Logo.svg" height="50px" width="50px" alt="logo"/>
+    </div>
+    <?php
+    //                $w = window.innerWidth;
+    //            if(w < 650px)
+    //            {
+    //                    echo
+    //                    '/*SVG*/';
+    //
+    //            }
+    //            else
+    //            {
+    //                echo    '
+    //                <a href="" id="hmenu"><strong>Home</strong></a>
+    //                <a href="" id="hmenu"><strong>News</strong></a>
+    //                <a href="" target="_blank" id="hmenu"><strong>Forum</strong></a>
+    //                <a href="" target="_blank" id="hmenu"><strong>Shop</strong></a>
+    //                <a href="" class="btn rounded-pill">Login</a> ';
+    //            }
+    ?>
+</header>
