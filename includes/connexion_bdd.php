dc7698d60ada6f8079396335c5e62448c007cbb6
Saiti91 <106150763+Saiti91@users.noreply.github.com>
2023-06-28 12:15:32 +0200

    1

--- includes/connexion_bdd.php
+++ includes/connexion_bdd.php
@@ -1,7 +1,8 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=51.178.30.38;port=3306;dbname=MasterTheWeb', 'esgi', 'password', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (Exception $e) {
-    die($e->getMessage());
+    $bdd = new PDO('mysql:host=51.178.30.38;port=3306;dbname=MasterTheWeb', 'esgi', 'DB%8T1N1NE&wZ64g4DB@', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
 }
+
 ?>
